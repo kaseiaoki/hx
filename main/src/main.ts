4766ef658fd1430ae308857c101e806f6a6ca554
aucuba <aoki.11235813@ezweb.ne.jp>
2018-03-27 10:32:14 +0900

    add class

--- main/src/main.ts
+++ main/src/main.ts
@@ -1,6 +1,16 @@
 
 let output_text :string = "";
 let index :number = 1;
+class text_write {
+  output_text: string;
+  text_index: number;
+  constructor(inputs: string) {
+     this.output_text = inputs+"\r";
+   }
+   output() {
+    console.log(this.output_text);
+  }
+}
 enum hx{
     h2,
     h3,
@@ -14,25 +24,30 @@ class EventName {
     static CLICK:string = "click";
     static MOUSE_MOVE:string = "mousemove";
 }
-
+function add_tags(splited:Array<string>){
+  let add_tag_text : string;
+  splited.forEach(text => {
+      if(text.match(hxs[hx.h2])!=null){
+        add_tag_text +="<h2>"+text+"</h2>";
+      }else if(text.match(hxs[hx.h3])!=null){
+        add_tag_text +="<h3>"+text+"</h3>";
+      }else if(text.match(hxs[hx.h4])!=null){
+        add_tag_text +="<h4>"+text+"</h4>";
+      }else{
+        add_tag_text +=text+"\r";
+      }
+      // }else if(text.match(hxs[hx.h5])!=null){
+      //   text="<h5>"+text+"</h5>";
+      // }
+  })
+    return add_tag_text;
+}
 const doc = document.getElementById('wordbutton');
 doc.addEventListener(EventName.CLICK, function () {
   console.log("hogehoge");
   let input_text : string =  (<HTMLInputElement>document.getElementById("q_area")).value;
   let splited_text : Array<string>= input_text.split(/\r\n|\r|\n/);
   const test=["1.","(1)","①","・1"];
-  splited_text.forEach(text => {
-      if(text.match(hxs[hx.h2])!=null){
-        text="<h2>"+text+"</h2>";
-      }else if(text.match(hxs[hx.h3])!=null){
-        text="<h3>"+text+"</h3>";
-      }else if(text.match(hxs[hx.h4])!=null){
-        text="<h4>"+text+"</h4>";
-      }
-      // }else if(text.match(hxs[hx.h5])!=null){
-      //   text="<h5>"+text+"</h5>";
-      // }
-     output_text += text+"\r";
-  });
-  console.log(output_text)
+  let  t = new text_write(add_tags(splited_text));
+  t.output();
 });
